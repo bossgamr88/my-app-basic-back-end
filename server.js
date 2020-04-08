45b166d6e6868ff9ee178d47431154a4a6e2743f
pakawat <pakawatkut@gmail.com>
2020-04-09 04:17:16 +0700

    Stap6: CRUD with Mongoose , Api done

--- server.js
+++ server.js
@@ -1,46 +1,53 @@
 const express = require('express')
 const app = express()
 const mongoose = require('mongoose')
-
+const Product = require('./models/product')
 
 mongoose.connect('mongodb://localhost:27017/node-api-101',{useNewUreParser:true})
+mongoose.connection.on('error',err =>{
+	console.error('MongoDB error',err)
+})
 
-
-// สร้าง database schema
-const Cat = mongoose.model('Cat',{name:String})
-
-// สร้าง instance จาก model 
-const kitty = new Cat({name:'JavaScript'})
-
-// save ลง database (return เป็น Promise)
-kitty.save().then(()=> console.log('meow'))
+// mock data  
+const products = [{}]
 
 
 app.use(express.json())
 
-app.get('/products',(req,res)=>{
+app.get('/products',async(req,res)=>{
+	const products = await Product.find({})
 	res.json(products)
 })
 
-app.get('/products/:id',(req,res)=>{
+app.get('/products/:id',async(req,res)=>{
 	const {id} = req.params
-	const result = products.find(product => product.id === id)
-	res.json(result)
+	// const result = products.find(product => product.id === id)
+	// res.json(result)
+	const product = await Product.findById(id)
+	res.json(product)
 })
 
-app.post('/products',(req,res)=>{
+app.post('/products',async (req,res)=>{
 	const payload = req.body
-	res.json(payload)
+	const product = new Product(payload)
+	await product.save()
+	res.status(201).end()
+	// res.json(payload)
 })
 
-app.put('/products/:id',(req,res)=>{
+app.put('/products/:id',async(req,res)=>{
+	const payload = req.body
 	const { id } = req.params
-	res.json({id})
+	// res.json({id})
+	const product = await Product.findByIdAndUpdate(id,{$set:payload})
+	res.json(product)
 })
 
-app.delete('/products/:id',(req,res)=>{
+app.delete('/products/:id',async(req,res)=>{
 	const { id } = req.params
-	res.json({id})
+	// res.json({id})
+	await Product.findByIdAndDelete(id)
+	res.status(204).end()
 })
 
 
