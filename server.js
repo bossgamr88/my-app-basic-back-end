5b015cd4f988fb50d418048848cb0bc1f61aaffa
pakawat <pakawatkut@gmail.com>
2020-04-09 01:45:04 +0700

    API and Mock data ,Test Api Postman

--- server.js
+++ server.js
@@ -1,11 +1,53 @@
 const express = require('express')
 const app = express()
 
-app.get('/hello/:message',(req,res)=>{
-	const {params} = req
-	res.json({message:'Mange!!',params}) 
-	//http://localhost:9000/hello/this-is-message
-	//http://localhost:9000/hello/pakawat
+app.use(express.json())
+
+// mock data 
+const products = [{
+	id:'1001',
+	name:'Node 101 ',
+	category:'Node',
+	price : 990
+},
+{
+	id:'1002',
+	name:'React 101',
+	category:'React',
+	price : 9990
+},
+{
+	id:'1003',
+	name:'MongoDB 101',
+	category:'MongoDB',
+	price : 1990
+},
+]
+
+app.get('/products',(req,res)=>{
+	res.json(products)
 })
+
+app.get('/products/:id',(req,res)=>{
+	const {id} = req.params
+	const result = products.find(product => product.id === id)
+	res.json(result)
+})
+
+app.post('/products',(req,res)=>{
+	const payload = req.body
+	res.json(payload)
+})
+
+app.put('/products/:id',(req,res)=>{
+	const { id } = req.params
+	res.json({id})
+})
+
+app.delete('/products/:id',(req,res)=>{
+	const { id } = req.params
+	res.json({id})
+})
+
 
 app.listen(9000,()=> console.log('Server run port 9000'))
